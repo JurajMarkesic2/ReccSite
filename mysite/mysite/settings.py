b639c774c52965c37d71e83d78598783707cbc3d
TroLLarch <jurajmarkesic@gmail.com>
2016-07-12 15:55:12 +0200

    creating folders, templates, static

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+        #default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+        #made
+    'general'        
+
+        #installed
+        
 ]
 
 MIDDLEWARE_CLASSES = [
