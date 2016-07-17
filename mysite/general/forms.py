177f7d8d0998a57f0a7cf85d6a3f4602bae4d1e2
OptimusLudio <stanislav.pavlic1@gmail.com>
2016-07-17 23:08:07 +0200

    Added models

--- mysite/general/forms.py
+++ mysite/general/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from django.forms.widgets import RadioSelect
 
 class quizForm(forms.Form):
-	firstQ = forms.BooleanField(label='First question',required=False,widget=RadioSelect(choices=[(True, 'yes'), 
-                                                            (False, 'no')]))
-	secondQ = forms.BooleanField(label='Second question',required=False,widget=RadioSelect(choices=[(True, 'yes'), 
-                                                            (False, 'no')]))
-	thirdQ = forms.BooleanField(label='Third question',required=False,widget=RadioSelect(choices=[(True, 'yes'), 
-                                                            (False, 'no')]))
-	fourthQ = forms.BooleanField(label='Forth question',required=False,widget=RadioSelect(choices=[(True, 'yes'), 
-                                                            (False, 'no')]))
+	firstQ = forms.BooleanField(label='First question',required=False,widget=RadioSelect(choices=[(True, 'Extrovert'), 
+                                                            (False, 'Introvert')]))
+	secondQ = forms.BooleanField(label='Second question',required=False,widget=RadioSelect(choices=[(True, 'Sensing'), 
+                                                            (False, 'Intuitive')]))
+	thirdQ = forms.BooleanField(label='Third question',required=False,widget=RadioSelect(choices=[(True, 'Thinking'), 
+                                                            (False, 'Feeling')]))
+	fourthQ = forms.BooleanField(label='Forth question',required=False,widget=RadioSelect(choices=[(True, 'Judging'), 
+                                                            (False, 'Perceiving')]))
