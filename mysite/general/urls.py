b36ce7783d021cc0882e37a871cfd4156b9c386a
OptimusLudio <stanislav.pavlic1@gmail.com>
2016-07-18 11:40:24 +0200

    Adding content

--- mysite/general/urls.py
+++ mysite/general/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),  #home page url
     url(r'^(?P<id>\d+)/$', views.result, name='result'), #result url, uses result's pk as a slug
     url(r'^about/$', views.about, name='about'), #about page url
-    #url(r'^contact/', views.contact, name='contact'), #contact page url
+    url(r'^contact/', views.contact, name='contact'), #contact page url
 ]
