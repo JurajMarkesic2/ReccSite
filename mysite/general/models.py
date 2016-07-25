519c0973fc6602f7b498450ef7377a3c7fa6dbae
TroLLarch <jurajmarkesic@gmail.com>
2016-07-25 19:19:03 +0200

    logo and typ img fix

--- mysite/general/models.py
+++ mysite/general/models.py
@@ -7,7 +7,7 @@ class Result(models.Model):
 	
 	description = models.TextField() #short description of their personality and what kind of content they like
 
-	typeImg = models.URLField(blank=True, null=True)
+	typeImg = models.URLField(blank=True, null=True, default="https://openclipart.org/image/2400px/svg_to_png/250864/Chess-Pieces-Lineup.png")
 
 	movies_name1 = models.CharField(max_length=160, default="name")  #name of the movie/book/song/tv-show
 	movies_link1 = models.URLField(default="https://www.amazon.com")      #our affiliate link to that movie/book/song/tv-show on Amazon,iTunes, etc...
